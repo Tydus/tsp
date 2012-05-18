68f3f8fbf99a019e0d1018d5c422874a2ac86c72
Tydus <Tydus@Tydus.org>
2012-05-18 17:03:01 +0800

    Fix: move log to main file

--- common.py
+++ common.py
@@ -15,17 +15,3 @@ class JsonRequestHandler(RequestHandler):
             if not self.current_user or lower(self.get_secure_cookie('t'))!=url[0]:
                 raise HTTPError(403)
 
-def strtime(tm=None):
-    return "%04d-%02d-%02d %02d:%02d:%02d"%gmtime(tm or time())[:6]
-
-# Log
-LOG_FATAL=0
-LOG_ERROR=1
-LOG_WARNING=2
-LOG_INFO=3
-LOG_VERBOSE=4
-def log(level,s,*args):
-    if level<=verbosity:
-        print >>logfd,"[%19s][%d]"%(strtime(),level)+s%tuple(args)
-        logfd.flush()
-
