dd99b5ef97f513dcc8b78f7181c391f618b29bf5
Tydus <Tydus@Tydus.org>
2012-05-18 14:07:35 +0800

    Implemented User Login and Logout

--- user.py
+++ user.py
@@ -5,7 +5,7 @@ from common import JsonRequestHandler
 # Model
 
 class User(Document):
-    username=StringField(required=True)
+    username=StringField(required=True,primaryKey=True)
     password=StringField(required=True)
     realname=StringField(required=True)
 
@@ -22,10 +22,25 @@ class Admin(User):
 # View
 class Login(JsonRequestHandler):
     def post(self):
-        raise NotImplementedError
-
+        '''
+        username=<>&password=<>
+        '''
+        if self.get_secure_cookie('u'):
+            return {'err':0}
+        u=User.get(username=self.get_argument('username'))
+        if not u:
+            return {'err':1,'desc':'No such user'}
+        if u.password!=self.get_argument('password'):
+            return {'err':1,'desc':'Password mismatch'}
+        self.set_secure_cookie('u',u.username)
+        for i in [Student,Professor,Admin]:
+            if isinstance(u,i):
+                self.set_secure_cookie('t',i.__name__)
+        return {'err':0}
 
 class Logout(JsonRequestHandler):
     def post(self):
-        raise NotImplementedError
+        self.clear_cookie('u')
+        self.clear_cookie('t')
+        return {'err':0}
 
