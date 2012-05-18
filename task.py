2b5b6eb7861db62cf750afa4a762018ee9176321
Tydus <Tydus@Tydus.org>
2012-05-18 15:57:19 +0800

    Rewrited task module

--- task.py
+++ task.py
@@ -12,15 +12,44 @@ class Task(Document):
     
 
 # View
-class ChooseTask(StudentRequestHandler):
-    def post(self):
+class Stu_Task(JsonRequestHandler):
+    '''/stu/task'''
+    def get(self):
         raise NotImplementedError
 
-class ChooseStudent(ProfessorRequestHandler):
-    def post(self):
+class Stu_Task_(JsonRequestHandler):
+    '''/stu/task/([^/]+)'''
+    def get(self,t):
+        raise NotImplementedError
+    def post(self,t):
         raise NotImplementedError
 
-class AssignStudent(AdminRequestHandler):
-    def post(self):
+class Pro_Task(JsonRequestHandler):
+    '''/pro/task'''
+    def get(self):
+        raise NotImplementedError
+
+class Pro_Task_(JsonRequestHandler):
+    '''/pro/task/([^/]+)'''
+    def get(self,t):
+        raise NotImplementedError
+    def post(self,t,s):
+        raise NotImplementedError
+
+class Admin_Student(JsonRequestHandler):
+    '''/admin/student'''
+    def get(self):
+        raise NotImplementedError
+
+class Admin_Task(JsonRequestHandler):
+    '''/admin/task'''
+    def get(self):
+        raise NotImplementedError
+
+class Admin_Task_(JsonRequestHandler):
+    '''/admin/task/([^/]+)'''
+    def get(self,t):
+        raise NotImplementedError
+    def post(self,t):
         raise NotImplementedError
 
