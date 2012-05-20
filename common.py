2d72a85eb1e12759ae13c429986922bab8fd4aec
Tydus <Tydus@Tydus.org>
2012-05-20 16:09:10 +0800

    Remove prepare

--- common.py
+++ common.py
@@ -1,5 +1,5 @@
 
-from tornado.web import RequestHandler,HTTPError
+from tornado.web import RequestHandler
 from tornado.escape import json_encode
 
 leafHandlers=[]
@@ -13,16 +13,3 @@ def leafHandler(path):
 class JsonRequestHandler(RequestHandler):
     def get_current_user(self):
         return self.get_secure_cookie('u')
-
-    '''
-    def write(self,obj):
-        return RequestHandler.write(self,json_encode(obj))
-    '''
-
-    def prepare(self):
-        uri=self.request.path.split('/')[1:]
-        print uri
-        if uri[0] in ['student','professor','admin']:
-            if not self.current_user or lower(self.get_secure_cookie('t'))!=uri[0]:
-                raise HTTPError(403)
-
