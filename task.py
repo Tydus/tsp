a7588e161d8c1f197930105c9ef6d55ac7d44c41
Tydus <Tydus@Tydus.org>
2012-05-20 18:15:23 +0800

    Finish assign task, not tested yet

--- task.py
+++ task.py
@@ -25,4 +25,43 @@ class Task_(JsonRequestHandler):
                 'stu':[{'name':x.realname} for x in i.students],
                 })
         return self.write({'task':l})
+    def post(self):
+        t=self.get_secure_cookie('t')
+        if t=='stu':
+            task=self.get_argument('task')
+
+            d=Task.objects(_id=ObjectId(task)).first()
+            if not d:
+                return self.write({'err':'Task not Exist'})
+            d.students.append(Student.objects(username=self.get_secure_cookie('u').first()))
+            d.save()
+
+        if t=='pro':
+            task=self.get_argument('task')
+            choice=int(self.get_argument('choice'))
+
+            d=Task.objects(_id=ObjectId(task)).first()
+            if not d:
+                return self.write({'err':'Task not Exist'})
+            if choice>=len(d.students):
+                return self.write({'err':'Out of Range'})
+            d.students=[d.students[choice]]
+            d.save()
+
+        if t=='admin':
+            task=self.get_argument('task')
+            stu=self.get_argument('stu')
+            d=Task.objects(_id=ObjectId(task)).first()
+            if not d:
+                return self.write({'err':'Task not Exist'})
+            if d.students:
+                return self.write({'err':'Already has Assignee'})
+            s=Student.objects(_id=stu).first()
+            if not s:
+                return self.write({'err':'No Such Student'})
+            d.students=[s]
+            d.save()
+
+
+        return self.write({})
 
