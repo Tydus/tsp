20914661adf1bdcb8fc41e6f8a81743c79be7125
Tydus <Tydus@Tydus.org>
2012-05-22 21:43:25 +0800

    Fix: admin can login at any time

--- user.py
+++ user.py
@@ -20,7 +20,7 @@ class Professor(User):
 class Admin(User):
     pass
 
-allow_phase={'stu':[0,2],'pro':[1,3],'admin':[4]}
+allow_phase={'stu':[0,2],'pro':[1,3],'admin':range(4)}
 
 # View
 @leafHandler(r'''/login''')
