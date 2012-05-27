f32a604e4d09d6b147b1acc8609df581d21acbab
Tydus <Tydus@Tydus.org>
2012-05-27 16:41:08 +0800

    Fix bug: User->Student, User->Professor

--- task.py
+++ task.py
@@ -20,7 +20,7 @@ class AddTask(JsonRequestHandler):
             return self.write({'err':'Not Your Turn'})
 
         try:
-            user=User.objects(username=self.get_secure_cookie('u')).first()
+            user=Professor.objects(username=self.get_secure_cookie('u')).first()
         except:
             return self.write({'err':'No Such User'})
 
@@ -55,11 +55,8 @@ class Task_(JsonRequestHandler):
 
     def post(self):
         phase=Settings.objects().first().phase
-        try:
-            user=User.objects(username=self.get_secure_cookie('u')).first()
-        except:
+        if not User.objects(username=self.get_secure_cookie('u')).first():
             return self.write({'err':'No Such User'})
-
 
         t=self.get_secure_cookie('t')
         if t=='stu':
@@ -68,6 +65,8 @@ class Task_(JsonRequestHandler):
             if phase not in [1,3]:
                 return self.write({'err':'Not Your Turn'})
 
+            s=Student.objects(username=self.get_secure_cookie('u')).first()
+
             d=Task.objects(id=ObjectId(task)).first()
             if not d:
                 return self.write({'err':'Task not Exist'})
@@ -75,9 +74,9 @@ class Task_(JsonRequestHandler):
                 return self.write({'err':'Task Full'})
 
             # Clear Currently Selected
-            Task.objects.update(pull__students=user)
+            Task.objects.update(pull__students=s)
 
-            d.students.append(user)
+            d.students.append(s)
             d.save()
 
         if t=='pro':
@@ -90,8 +89,6 @@ class Task_(JsonRequestHandler):
             d=Task.objects(id=ObjectId(task)).first()
             if not d:
                 return self.write({'err':'Task not Exist'})
-            if d.professor!=user:
-                return self.write({'err':'Not Your Task'})
             for i in d.students:
                 if i.username==choice:
                     c=i
