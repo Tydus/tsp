71747d330fd2afee309be9f918561a4d6316af9c
Tydus <Tydus@Tydus.org>
2012-07-01 08:25:39 +0800

    manually ResetDB in test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from util import passwordHash
+from util import passwordHash,resetDB
 import json_rpc
 
 Verbose=True
@@ -42,8 +42,12 @@ class Session(json_rpc.Json_RPC):
         except Exception,e:
             return (criteria==Error,"%s: %s"%(str(type(e)),e.message))
 
+# Reset DB
+resetDB()
 
+# Test Script Starts Here
 admin=Session()
+
 admin.test('/login','Admin Login',{"role":"Admin"},username='admin',password=passwordHash('admin','admin'))
 admin.test('/profile','Admin profile',lambda r:r['role']=='Admin')
 admin.test('/chpasswd','Admin Change Password',{},password=passwordHash('admin','admin'),new_password=passwordHash('admin','test'))
